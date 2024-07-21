1bee65d02ab781138aad7adf29a4d66ed4f2b3ea
Harikajonnada <128477278+Harikajonnada@users.noreply.github.com>
2024-07-21 19:58:00 +0530

    day2

---
+++ day2_ques_8.py
@@ -0,0 +1,21 @@
+# given spce sep integer list find the avg of elem prst in even index
+'''my_list=list(map(int,input().split(" ")))
+sum=0
+count=0
+for i in range(len(my_list)):
+    if(i%2==0):
+        sum+=my_list[i]
+        count+=1
+print(sum/count)'''
+
+
+#n=int(input())
+#i=1
+#for i in range(n):
+   # i+=1
+   # print(i,end=" ")
+list=[]
+n=int(input())
+for i in range(1,n+1):
+    list.append(i)
+    print(list)
